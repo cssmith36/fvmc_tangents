390b1b0f7a69ad38e66a6956502b5aea0a46359d
Yixiao Chen <yixiaoc@princeton.edu>
2023-04-16 01:08:09 -0400

    refactor wavefunction

--- fvmc/wavefunction.py
+++ fvmc/wavefunction.py
@@ -1,3 +1,4 @@
+import abc
 from dataclasses import field as _field
 from typing import Optional, Sequence, Tuple, Union
 
@@ -10,6 +11,32 @@ from .utils import Array, _t_real, build_mlp, cdist, diffmat, fix_init, pdist
 
 def log_prob_from_model(model: nn.Module):
     return lambda p, *args, **kwargs: 2 * model.apply(p, *args, **kwargs)[1]
+
+
+class FullWfn(nn.Module, abc.ABC):
+    def __call__(self, r: Array, x: Array) -> Tuple[Array, Array]:
+        """Take ion position r and electron position x, return sign and log|psi|"""
+        raise NotImplementedError
+    
+
+class ElecWfn(nn.Module, abc.ABC):
+    def __call__(self, x: Array) -> Tuple[Array, Array]:
+        """Take only the electron position x, return sign and log|psi|"""
+        raise NotImplementedError
+
+
+class FixIons(ElecWfn):
+    r"""Module warpper that fix the ion positions for a full model
+    
+    This class takes a full wavefunction model f(r,x) of r (ions) and x (electrons)
+    and the fixed ion positions r_0, and return a new model which only depends on x.
+    Think it as a partial warpper that works on nn.Module
+    """
+    model: FullWfn
+    ions: Array
+
+    def __call__(self, x: Array) -> Tuple[Array, Array]:
+        return self.model(r=self.ions, x=x)
 
 
 # follow the TwoBodyExpDecay class in vmcnet
@@ -25,23 +52,22 @@ class Jastrow(nn.Module):
     Z_j and Q are parameters that are initialized to be ion charges and 1.
     """
 
-    ions: Array
     elems: Array
 
     @nn.compact
-    def __call__(self, x: Array) -> Array:
+    def __call__(self, r: Array, x: Array) -> Array:
         # calculate initial scale, so that it returns 0 if all electrons are on ions
         cmat = jnp.expand_dims(self.elems, -1) * jnp.expand_dims(self.elems, -2)
-        scale = 0.5 * jnp.sum(pdist(self.ions) * cmat)
+        scale = 0.5 * jnp.sum(pdist(r) * cmat)
         # make z and q parameters
         z = self.param("z", fix_init, self.elems, _t_real)
         q = self.param("q", fix_init, 1.0, _t_real)
         # distance matrices
-        r_ei = cdist(x, self.ions)
-        r_ee = pdist(x)
+        d_ei = cdist(x, r)
+        d_ee = pdist(x)
         # interaction terms
-        corr_ei = jnp.sum(r_ei * z, axis=-1)
-        corr_ee = jnp.sum(jnp.triu(r_ee) * q, axis=-1)
+        corr_ei = jnp.sum(d_ei * z, axis=-1)
+        corr_ee = jnp.sum(jnp.triu(d_ee) * q, axis=-1)
         return jnp.sum(corr_ee - corr_ei, axis=-1) + scale
 
     
@@ -52,22 +78,21 @@ class SimpleOrbital(nn.Module):
     and output a vector of size n_orb, correspinding to \psi_k(x_i)
     """
 
-    ions: Array
     n_orb: int
     n_hidden: int = 3
     activation: str = "gelu"
 
     @nn.compact
-    def __call__(self, x: Array) -> Array:
+    def __call__(self, r: Array, x: Array) -> Array:
         n_el = x.shape[-2]
         n_dim = x.shape[-1]
-        n_ion = self.ions.shape[-2]
+        n_ion = r.shape[-2]
         n_feature = n_ion * (n_dim + 1)
         resnet = build_mlp(
             [n_feature]*self.n_hidden + [self.n_orb], 
             residual=True, activation=self.activation, param_dtype=_t_real)
         # build input features
-        diff_ei = diffmat(x, self.ions) # [..., n_el, n_ion, 3]
+        diff_ei = diffmat(x, r) # [..., n_el, n_ion, 3]
         r_ei = jnp.linalg.norm(diff_ei, axis=-1, keepdims=True) # [..., n_el, n_ion, 1]
         feature = jnp.concatenate([diff_ei, r_ei], axis=-1) # [..., n_el, n_ion, 4]
         feature = feature.reshape(*feature.shape[:-2], n_feature) # [..., n_el, (n_ion * 4)]
@@ -75,7 +100,7 @@ class SimpleOrbital(nn.Module):
         return resnet(feature) # [..., n_el, n_orb]
 
 
-class Slater(nn.Module):
+class Slater(FullWfn):
     r"""Slater determinant from single particle orbitals
     
     Separate the electrons into different spins and calculate orbitals for both.
@@ -83,15 +108,13 @@ class Slater(nn.Module):
     Return sign and log of determinant when called.
     """
 
-    ions: Array
-    elems: Array
     spin: Optional[int] = None # difference between alpha and beta spin
     full_det: bool = True
     orbital_type: str = "simple"
     orbital_args: dict = _field(default_factory=dict)
 
     @nn.compact
-    def __call__(self, x: Array) -> Tuple[Array, Array]:
+    def __call__(self, r: Array, x: Array) -> Tuple[Array, Array]:
         n_el = x.shape[-2]
         if self.spin is None:
             n_up, n_dn = n_el//2, n_el - n_el//2
@@ -104,11 +127,11 @@ class Slater(nn.Module):
             OrbCls = SimpleOrbital
         else:
             raise ValueError("unsupported orbital type")
-        orb_up_fn = OrbCls(self.ions, (n_el if self.full_det else n_up), **self.orbital_args)
-        orb_dn_fn = OrbCls(self.ions, (n_el if self.full_det else n_dn), **self.orbital_args)
+        orb_up_fn = OrbCls((n_el if self.full_det else n_up), **self.orbital_args)
+        orb_dn_fn = OrbCls((n_el if self.full_det else n_dn), **self.orbital_args)
 
-        orb_up = orb_up_fn(x[..., :n_up, :]) # [..., n_up, n_el|n_up]
-        orb_dn = orb_dn_fn(x[..., n_up:, :]) # [..., n_up, n_el|n_dn]
+        orb_up = orb_up_fn(r, x[..., :n_up, :]) # [..., n_up, n_el|n_up]
+        orb_dn = orb_dn_fn(r, x[..., n_up:, :]) # [..., n_up, n_el|n_dn]
 
         if self.full_det:
             orb_full = jnp.concatenate([orb_up, orb_dn], axis=-2)
@@ -119,7 +142,7 @@ class Slater(nn.Module):
             return sign_up * sign_dn, ldet_up + ldet_dn
 
 
-class ProductModel(nn.Module):
+class ProductModel(FullWfn):
     r"""Pruduct of multiple model results.
     
     Assuming the models returns in log scale. 
@@ -131,13 +154,13 @@ class ProductModel(nn.Module):
     submodels: Sequence[nn.Module]
 
     @nn.compact
-    def __call__(self, x: Array) -> Tuple[Array, Array]:
+    def __call__(self, r:Array, x: Array) -> Tuple[Array, Array]:
         sign = 1.
         logf = 0.
         with_sign = True # False will make the sign optional
 
         for model in self.submodels:
-            result = model(x)
+            result = model(r, x)
             if isinstance(result, tuple):
                 sign *= result[0]
                 logf += result[1]
@@ -154,8 +177,9 @@ class ProductModel(nn.Module):
 def build_jastrow_slater(ions, elems, spin=None, 
         full_det=True, orbital_type="simple", orbital_args=None):
     orbital_args = orbital_args or {}
-    jastrow = Jastrow(ions, elems)
-    slater = Slater(ions, elems, spin, full_det, orbital_type, orbital_args)
+    jastrow = Jastrow(elems)
+    slater = Slater(spin, full_det, orbital_type, orbital_args)
     model = ProductModel([jastrow, slater])
-    return model
+    elec_model = FixIons(model, ions)
+    return elec_model
     
--- tests/test_wavefunction.py
+++ tests/test_wavefunction.py
@@ -45,15 +45,15 @@ _key0 = jax.random.PRNGKey(0)
 
 def test_jastrow():
     ions, elems, x = make_collapse_conf()
-    jastrow = Jastrow(ions, elems)
-    params = jastrow.init(_key0, x)
+    jastrow = Jastrow(elems)
+    params = jastrow.init(_key0, ions, x)
     
-    actual_out = jastrow.apply(params, x)
+    actual_out = jastrow.apply(params, ions, x)
     assert actual_out.shape == (1,)
     np.testing.assert_allclose(actual_out[0], 0.)
 
     new_x = x.at[0,0,2].set(2e10)[0, :-2, :] # now remve batch
-    new_out = jastrow.apply(params, new_x)
+    new_out = jastrow.apply(params, ions, new_x)
     np.testing.assert_allclose(new_out, -3 * 2e10, rtol=1e-6)
 
 
@@ -61,26 +61,26 @@ def test_orbital_shape():
     ions, elems, x = make_collapse_conf()
     n_batch, n_el = x.shape[:-1]
     n_orb = 7
-    orbital = SimpleOrbital(ions, n_orb, n_hidden=1)
-    params = orbital.init(_key0, x)
+    orbital = SimpleOrbital(n_orb, n_hidden=1)
+    params = orbital.init(_key0, ions, x)
 
-    assert orbital.apply(params, x).shape == (n_batch, n_el, n_orb)
-    assert orbital.apply(params, x[0]).shape == (n_el, n_orb)
+    assert orbital.apply(params, ions, x).shape == (n_batch, n_el, n_orb)
+    assert orbital.apply(params, ions, x[0]).shape == (n_el, n_orb)
 
 
 @pytest.mark.parametrize("full_det,spin", [(True, None), (False, 1)])
 def test_slater_antisymm(full_det, spin):
     ions, elems, x = make_collapse_conf()
     n_batch, n_el = x.shape[:-1]
-    slater = Slater(ions, elems, full_det=full_det, orbital_args={"n_hidden": 1})
-    params = slater.init(_key0, x)
+    slater = Slater(full_det=full_det, orbital_args={"n_hidden": 1})
+    params = slater.init(_key0, ions, x)
 
     x = x + jax.random.normal(_key0, x.shape)
     iperm = jnp.arange(n_el, dtype=int).at[:2].set([1,0])
     px = x[:, iperm, :]
     
-    sign1, logf1 = slater.apply(params, x)
-    sign2, logf2 = slater.apply(params, px)
+    sign1, logf1 = slater.apply(params, ions, x)
+    sign2, logf2 = slater.apply(params, ions, px)
     assert sign1.shape == logf1.shape == (n_batch,)
     np.testing.assert_allclose(sign1, -sign2)
     np.testing.assert_allclose(logf1, logf2)
@@ -92,11 +92,11 @@ def test_jastrow_slater():
     n_el = x.shape[0]
     model = build_jastrow_slater(ions, elems, None, full_det=True, orbital_args={"n_hidden": 1})
     params = model.init(_key0, x)
-    subp0 = {"params": params["params"]["submodels_0"]}
-    subp1 = {"params": params["params"]["submodels_1"]}
+    subp0 = {"params": params["params"]['model']["submodels_0"]}
+    subp1 = {"params": params["params"]['model']["submodels_1"]}
 
     sign, logf = model.apply(params, x)
-    log_jas = model.submodels[0].apply(subp0, x)
-    sign_sla, log_sla = model.submodels[1].apply(subp1, x)
+    log_jas = model.model.submodels[0].apply(subp0, ions, x)
+    sign_sla, log_sla = model.model.submodels[1].apply(subp1, ions, x)
     np.testing.assert_allclose(sign, sign_sla)
     np.testing.assert_allclose(logf, log_jas + log_sla)
